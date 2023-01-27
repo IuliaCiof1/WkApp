245a7d9e38974eabfe6888ed1d469dd819b111ff
iuliaCiof <iulia.ciofindac@gmail.com>
2023-01-27 17:47:02 +0200

    added database

---
+++ App/Database/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using App.Features.Assignments.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Base.Database;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AssignmentModel> Assignments { get; set; }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,7 +1,13 @@
+using App.Base.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("Default");//
 
+// Add services to the container.
+//nu se da commit la appsettings.development.json
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
